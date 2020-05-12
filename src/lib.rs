3d6cc72577c2b620e9690432bacc24897ad869b8
Fabio Mencoboni <Fabio.Mencoboni@gmail.com>
2020-05-11 22:34:10 -0400

    added lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+mod nlp;
+pub use nlp::{text_token_bigrams, text_tokens};
+
+mod sparsevec;
+pub use sparsevec::cosine_similarity;
+
+mod hash;
+pub use hash::hash_string;
+
+mod web;
+pub use web::get_url;
+
